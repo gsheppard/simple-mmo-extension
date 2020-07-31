110eab1fa0df72846329b9f53b7ef0a33ee1f9f3
Gregory Sheppard <gregory.sheppard@gmail.com>
2020-07-31 18:35:17 -0500

    Complete v1

--- index.js
+++ index.js
@@ -86,14 +86,14 @@ const travel = () => {
 }
 const quest = () => {
     if (!isModalOpen()) {
-        // const uncompletedQuests = Array.from(document.getElementsByClassName('kt-widget5__item')).filter((e) => { return e.getElementsByClassName('label-success').length <= 0 });
-        // const quest = uncompletedQuests[uncompletedQuests.length - 1];
+        const uncompletedQuests = Array.from(document.getElementsByClassName('kt-widget5__item')).filter((e) => { return e.getElementsByClassName('label-success').length <= 0 });
+        const quest = uncompletedQuests[uncompletedQuests.length - 1];
 
-        const allQuests = Array.from(document.getElementsByClassName('kt-widget5__item'));
-        const quest = allQuests.find(e => e.getElementsByClassName('kt-widget5__title')[0].innerText === 'Help a blind man and his dog')
+        // const allQuests = Array.from(document.getElementsByClassName('kt-widget5__item'));
+        // const quest = allQuests.find(e => e.getElementsByClassName('kt-widget5__title')[0].innerText === 'Help a blind man and his dog')
         simulateClick(quest.getElementsByTagName('button')[0]);
     } else {
-        const performBtn = getModalButton('Perform quest');
+        const performBtn = getModalButton('Perform quest') || getModalButton('Perform Quest');
         const preparingQuest = getModalButton('Preparing quest...');
         const repeatBtn = getModalButton('Repeat quest');
         const error = document.getElementById('swal2-validation-message')?.innerText;
@@ -111,6 +111,23 @@ const quest = () => {
         }
     }
 }
+const jobs = () => {
+    const btn = Array.from(document.getElementsByClassName('btn-success')).find(b => 
+        b.innerText === 'Go to your job' || b.innerText === 'Start working'
+    );
+
+    if (isModalOpen()) {
+        const jobRange = document.getElementsByClassName('swal2-range')[0]
+        jobRange.value = '2'; // doesn't work
+        const startBtn = getModalButton('Start the job');
+        simulateClick(startBtn);
+    } else if (btn) {
+        simulateClick(btn);
+    }
+}
+const isJobActive = () => {
+    return !!Array.from(document.querySelectorAll('a.btn')).find(e => e.innerText === 'Go to your job area' || e.innerText === 'You are currently working');
+}
 
 let appInterval;
 const startInterval = () => {
@@ -122,6 +139,11 @@ const startInterval = () => {
 
         // closes levelup modal
         isModalOpen();
+
+        if (isJobActive()) {
+            stopIt(true);
+            return;
+        }
 
         if (window.location.pathname.match('/npcs/attack')) {
             attack();
@@ -144,44 +166,99 @@ const startInterval = () => {
                 travel();
             }
         } else {
-            if (window.location.pathname !== '/') {
-                window.location.replace('/');
+            if (window.location.pathname.match('/jobs/view')) {
+                jobs();
+            } else if (window.location.pathname !== '/jobs/viewall') {
+                window.location.replace('/jobs/viewall');
             }
         }
     }, 2000);
 };
 
-const start = () => {
-    const btn = document.getElementById('bot-play');
-
-    const startIt = (e) => {
-        console.log('starting...');
-        startInterval();
-        btn.textContent = 'Stop';
-        btn.classList.remove('btn-success');
-        btn.classList.add('btn-danger');
-        localStorage.setItem('smmo-ext', 'true')
-    }
-    const stopIt = (e) => {
-        console.log('stopping...');
-        clearInterval(appInterval);
-        appInterval = undefined;
-        btn.textContent = 'Start';
-        btn.classList.remove('btn-danger');
-        btn.classList.add('btn-success');
-        localStorage.removeItem('smmo-ext')
-    }
+const stopJobInterval = () => {
+    const jobsBtn = document.getElementById('bot-jobs');
+    const appBtn = document.getElementById('bot-play');
     
-    btn.onclick = (e) => {
-        if (appInterval) {
-            stopIt(e);
+    clearInterval(jobInterval);
+    jobsBtn.textContent = 'Jobs: Start';
+    jobsBtn.classList.remove('btn-danger');
+    jobsBtn.classList.add('btn-info');
+    appBtn.disabled = false;
+
+    localStorage.removeItem('smmo-ext-jobs');
+}
+
+let jobInterval;
+const startJobInterval = () => {
+    const jobsBtn = document.getElementById('bot-jobs');
+    const appBtn = document.getElementById('bot-play');
+    jobsBtn.textContent = 'Jobs: Stop';
+    jobsBtn.classList.remove('btn-info');
+    jobsBtn.classList.add('btn-danger');
+    appBtn.disabled = true;
+    localStorage.setItem('smmo-ext-jobs', 'true');
+
+    jobInterval = setInterval(() => {
+        if (isJobActive()) {
+            window.location.reload(false);
         } else {
-            startIt(e);
+            stopJobInterval();
+
+            startIt();
+        }
+    }, 60000);
+}
+
+const startIt = () => {
+    const btn = document.getElementById('bot-play');
+    console.log('starting...');
+    startInterval();
+    
+    btn.textContent = 'Auto: Stop';
+    btn.classList.remove('btn-success');
+    btn.classList.add('btn-danger');
+    localStorage.setItem('smmo-ext', 'true')
+}
+const stopIt = (turnJobWaitOn) => {
+    const btn = document.getElementById('bot-play');
+    console.log('stopping...');
+    clearInterval(appInterval);
+    appInterval = undefined;
+    
+    btn.textContent = 'Auto: Start';
+    btn.classList.remove('btn-danger');
+    btn.classList.add('btn-success');
+    localStorage.removeItem('smmo-ext');
+
+    if (turnJobWaitOn) {
+        startJobInterval();
+    }
+}
+
+const start = () => {
+    const startBtn = document.getElementById('bot-play');
+    const jobsBtn = document.getElementById('bot-jobs');
+    
+    startBtn.onclick = (e) => {
+        if (appInterval) {
+            stopIt();
+        } else {
+            startIt();
         }
     };
+
+    jobsBtn.onclick = () => {
+        if (jobInterval) {
+            stopJobInterval();
+        } else {
+            startJobInterval();
+        }
+    }
     
     if (localStorage.getItem('smmo-ext') === 'true') {
         startIt();
+    } else if (localStorage.getItem('smmo-ext-jobs') === 'true') {
+        startJobInterval();
     }
 };
 
@@ -189,12 +266,15 @@ const controlDiv = document.createElement('div');
 controlDiv.innerHTML = `
 <div class="container" style="background-color: white; padding: 15px;">
     <div class="row">
-        <div class="col-md-10">
+        <div class="col-md-9">
             <h4>SimpleMMO Bot</h4>
         </div>
-        <div class="col-md-2">
-            <button class="btn btn-info" name="action" id="bot-play">
-                Start
+        <div class="col-md-3">
+            <button class="btn btn-info" name="action" id="bot-jobs">
+                Jobs: Start
+            </button>
+            <button class="btn btn-success" name="action" id="bot-play">
+                Auto: Start
             </button>
         </div>
     </div>
